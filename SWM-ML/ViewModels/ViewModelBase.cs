5a2d59e3deb27f225c498503a2fa2b0bd4524fbf
Jeremy Potvin <Potvin98@LIVE.CA>
2022-01-14 11:19:16 -0500

    Initial commit

---
+++ SWM-ML/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWM_ML.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
