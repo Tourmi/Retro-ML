2514501f5946580619c04569bd2e2b1a595393b5
Tourmi <domino_b10@hotmail.com>
2022-01-17 15:41:54 -0500

    Cleaned up Arduino code

--- ArduinoSNESController/src/main.cpp
+++ ArduinoSNESController/src/main.cpp
@@ -13,39 +13,50 @@
 #define S 29 //Start
 #define s 30 //Select
 
-byte* bytes = new byte[2];
+#define MIN_PORT 22
+#define MAX_PORT 33
+#define TEST_MODE false
 
-void setup() {
+byte *bytes = new byte[2];
+
+void setup()
+{
   Serial.begin(9600);
-  for(int i = 22; i <= 33; i++) {
+  for (int i = MIN_PORT; i <= MAX_PORT; i++)
+  {
     pinMode(i, OUTPUT);
   }
 }
 
-void loop() {
-  Serial.readBytes(bytes, 2);
-  int curr = 0;
-  digitalWrite(A, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(B, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(X, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(Y, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(l, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(r, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(u, bytes[0] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(d, bytes[0] & 1 << curr++ ? HIGH : LOW);
-
-  curr = 0;
-  digitalWrite(L, bytes[1] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(R, bytes[1] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(S, bytes[1] & 1 << curr++ ? HIGH : LOW);
-  digitalWrite(s, bytes[1] & 1 << curr++ ? HIGH : LOW);
-
-  /*
-  for(int i = 0; i <= 33; i++) {
-    digitalWrite(i + 22, HIGH);
-    delay(500);
-    digitalWrite(i + 22, LOW);
-    delay(50);
+void loop()
+{
+  if (TEST_MODE)
+  {
+    for (int i = 0; i < MAX_PORT - MIN_PORT; i++)
+    {
+      digitalWrite(i + MIN_PORT, HIGH);
+      delay(500);
+      digitalWrite(i + MIN_PORT, LOW);
+      delay(50);
+    }
   }
-  */
+  else
+  {
+    Serial.readBytes(bytes, 2);
+    int curr = 0;
+    digitalWrite(A, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(B, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(X, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(Y, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(l, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(r, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(u, bytes[0] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(d, bytes[0] & 1 << curr++ ? HIGH : LOW);
+
+    curr = 0;
+    digitalWrite(L, bytes[1] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(R, bytes[1] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(S, bytes[1] & 1 << curr++ ? HIGH : LOW);
+    digitalWrite(s, bytes[1] & 1 << curr++ ? HIGH : LOW);
+  }
 }
