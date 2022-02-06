af9f3d99cc039da29984ff76b8dba312c28b7573
Dominic Brousseau <domino_b10@hotmail.com>
2022-02-06 13:11:19 -0500

    Slight refactor of the NetworkViewModel

--- SMW-ML/ViewModels/Neural/NetworkViewModel.cs
+++ SMW-ML/ViewModels/Neural/NetworkViewModel.cs
@@ -23,9 +23,35 @@ namespace SMW_ML.ViewModels.Neural
         {
             public static int NodeSize => NetworkViewModel.NodeSize;
             public static int GridSize => NetworkViewModel.GridSize;
-            public bool Active { get; set; }
-            public double PositionX { get; set; }
-            public double PositionY { get; set; }
+
+            public Node(bool active) : this(active, 0, 0) { }
+
+            public Node(bool active, double positionX, double positionY)
+            {
+                Active = active;
+                PositionX = positionX;
+                PositionY = positionY;
+            }
+
+            public bool Active { get; }
+            public double PositionX { get; }
+            public double PositionY { get; }
+        }
+
+        public struct Connection
+        {
+            public Connection(Point startPoint, Point endPoint, bool positive, double intensity)
+            {
+                LineStartPoint = startPoint;
+                LineEndPoint = endPoint;
+                Positive = positive;
+                Intensity = intensity;
+            }
+
+            public Point LineStartPoint { get; }
+            public Point LineEndPoint { get; }
+            public bool Positive { get; }
+            public double Intensity { get; }
         }
 
         public class NodeGroupViewModel : ViewModelBase
@@ -54,51 +80,11 @@ namespace SMW_ML.ViewModels.Neural
 
             public ObservableCollection<Node> Nodes { get; set; }
         }
-
-        public class ConnectionViewModel : ViewModelBase
-        {
-            private Point lineStartPoint;
-            private Point lineEndPoint;
-            private bool positive;
-            private double intensity;
-
-            public ConnectionViewModel(Point startPoint, Point endPoint, bool positive, double intensity)
-            {
-                LineStartPoint = startPoint;
-                LineEndPoint = endPoint;
-                Positive = positive;
-                Intensity = intensity;
-            }
-
-            public Point LineStartPoint
-            {
-                get => lineStartPoint;
-                set => this.RaiseAndSetIfChanged(ref lineStartPoint, value);
-            }
-            public Point LineEndPoint
-            {
-                get => lineEndPoint;
-                set => this.RaiseAndSetIfChanged(ref lineEndPoint, value);
-            }
-            public bool Positive
-            {
-                get => positive;
-                set => this.RaiseAndSetIfChanged(ref positive, value);
-            }
-            public double Intensity
-            {
-                get => intensity;
-                set
-                {
-                    this.RaiseAndSetIfChanged(ref intensity, value);
-                }
-            }
-        }
         public static int TotalWidth => 1800;
         public static int TotalHeight => 800;
 
-        public static int NodeSize => 14;
         public static int GridSize => 16;
+        public static int NodeSize => GridSize - 2;
         public static int LeftOffset => 100;
         public static int SpacingBetweenInputs => 5;
 
@@ -108,6 +94,9 @@ namespace SMW_ML.ViewModels.Neural
         private bool runningUpdate = false;
         private (int sourceNode, int targetNode, double weight)[][]? currConnectionLayers;
 
+        /// <summary>
+        /// Only exists so the XAML designer can work.
+        /// </summary>
         public NetworkViewModel() : this(new NeuralConfig())
         {
 
@@ -131,9 +120,14 @@ namespace SMW_ML.ViewModels.Neural
             }
 
             MiddleNodes = new ObservableCollection<Node>();
-            Connections = new ObservableCollection<ConnectionViewModel>();
+            Connections = new ObservableCollection<Connection>();
         }
 
+        /// <summary>
+        /// Updates the connections as well as the middle nodes of the neural network. Does not update them if the current topology is the same as the given one
+        /// </summary>
+        /// <param name="connectionLayers"></param>
+        /// <param name="outputIds"></param>
         public void UpdateTopology((int sourceNode, int targetNode, double weight)[][] connectionLayers, int[] outputIds)
         {
             if (runningTopologyUpdate) return;
@@ -177,23 +171,24 @@ namespace SMW_ML.ViewModels.Neural
                         if (!nodePositions.ContainsKey(sourceNode))
                         {
                             nodePositions[sourceNode] = new Point(middleStartX + i * middleXOffset, Random.Shared.NextDouble() * (TotalHeight - GridSize) + GridSize);
-                            MiddleNodes.Add(new Node() { PositionX = nodePositions[sourceNode].X, PositionY = nodePositions[sourceNode].Y });
+                            MiddleNodes.Add(new Node(false, nodePositions[sourceNode].X, nodePositions[sourceNode].Y));
                         }
 
                         connectionsToAdd.Add((sourceNode, targetNode, weight));
                     }
                 }
 
+                // Add the connections
                 foreach (var (source, target, weight) in connectionsToAdd)
                 {
                     //If the target doesn't exist, put it at the right
                     if (!nodePositions.ContainsKey(target))
                     {
                         nodePositions[target] = new Point(middleStartX + (currConnectionLayers.Length - 1) * middleXOffset, Random.Shared.NextDouble() * (TotalHeight - GridSize) + GridSize);
-                        MiddleNodes.Add(new Node() { PositionX = nodePositions[target].X, PositionY = nodePositions[target].Y });
+                        MiddleNodes.Add(new Node(false, nodePositions[target].X, nodePositions[target].Y));
                     }
 
-                    Connections.Add(new ConnectionViewModel(nodePositions[source] + middleOfNode, nodePositions[target] + middleOfNode, weight >= 0, Math.Abs(weight) / 5.0 + 0.3));
+                    Connections.Add(new Connection(nodePositions[source] + middleOfNode, nodePositions[target] + middleOfNode, weight >= 0, Math.Abs(weight) / 5.0 + 0.3));
                 }
 
                 runningTopologyUpdate = false;
@@ -242,7 +237,6 @@ namespace SMW_ML.ViewModels.Neural
             for (int i = 0; i < nodeGroup.Count; i++)
             {
                 var group = nodeGroup[i];
-
                 double currY = initialY;
 
                 for (int y = 0; y < group.GridHeight; y++)
@@ -264,48 +258,45 @@ namespace SMW_ML.ViewModels.Neural
             return result;
         }
 
+        /// <summary>
+        /// Updates the status of the nodes of the neural network. Note that for now, middle nodes are not updated.
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="outputs"></param>
         public void UpdateNodes(double[] inputs, double[] outputs)
         {
             if (runningUpdate) return;
             runningUpdate = true;
             Dispatcher.UIThread.Post(() =>
             {
-                int startIndex = 0;
                 using var delay = DelayChangeNotifications();
-                foreach (var nodeGroup in Inputs)
-                {
-                    for (int i = 0; i < nodeGroup.Nodes.Count; i++)
-                    {
-                        bool shouldBeActive = inputs[i + startIndex] > 0;
-                        if (nodeGroup.Nodes[i].Active != shouldBeActive)
-                        {
-                            nodeGroup.Nodes[i] = new Node() { Active = shouldBeActive };
-                        }
-                    }
-
-                    startIndex += nodeGroup.Nodes.Count;
-                }
-                startIndex = 0;
-                foreach (var nodeGroup in Outputs)
-                {
-                    for (int i = 0; i < nodeGroup.Nodes.Count; i++)
-                    {
-                        bool shouldBeActive = outputs[i + startIndex] > 0;
-                        if (nodeGroup.Nodes[i].Active != shouldBeActive)
-                        {
-                            nodeGroup.Nodes[i] = new Node() { Active = shouldBeActive };
-                        }
-                    }
-
-                    startIndex += nodeGroup.Nodes.Count;
-                }
+                UpdateNodes(Inputs, inputs);
+                UpdateNodes(Outputs, outputs);
                 runningUpdate = false;
             });
+        }
+        private void UpdateNodes(IEnumerable<NodeGroupViewModel> nodeGroups, double[] states)
+        {
+            int startIndex = 0;
+            foreach (var nodeGroup in nodeGroups)
+            {
+                for (int i = 0; i < nodeGroup.Nodes.Count; i++)
+                {
+                    bool shouldBeActive = states[i + startIndex] > 0;
+                    if (nodeGroup.Nodes[i].Active != shouldBeActive)
+                    {
+                        nodeGroup.Nodes[i] = new Node(shouldBeActive);
+                    }
+                }
+
+                startIndex += nodeGroup.Nodes.Count;
+            }
+
         }
 
         public ObservableCollection<NodeGroupViewModel> Inputs { get; }
         public ObservableCollection<Node> MiddleNodes { get; }
         public ObservableCollection<NodeGroupViewModel> Outputs { get; }
-        public ObservableCollection<ConnectionViewModel> Connections { get; }
+        public ObservableCollection<Connection> Connections { get; }
     }
 }
