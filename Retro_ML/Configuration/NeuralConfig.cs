aa1d799afd6e4c433b60ca59a78d66370e167670
Tourmi <domino_b10@hotmail.com>
2022-06-21 19:07:16 -0400

    Backend implementation of memory nodes
    Also includes updates in the Frontend ViewModel

--- Retro_ML/Configuration/NeuralConfig.cs
+++ Retro_ML/Configuration/NeuralConfig.cs
@@ -2,74 +2,113 @@
 using Retro_ML.Neural;
 using Retro_ML.Utils;
 
-namespace Retro_ML.Configuration
+namespace Retro_ML.Configuration;
+
+/// <summary>
+/// Configuration of the neural network. Modifying these values makes previous neural network incompatible with the new settings.
+/// </summary>
+public class NeuralConfig
 {
+    public int ShortTermMemoryNodeCount { get; set; } = 2;
+    public int LongTermMemoryNodeCount { get; set; } = 3;
+    public int PermanentMemoryNodeCount { get; set; } = 4;
+    public double MaximumMemoryNodeValue { get; set; } = 1.0;
+
     /// <summary>
-    /// Configuration of the neural network. Modifying these values makes previous neural network incompatible with the new settings.
+    /// The input nodes to use by the neural network
     /// </summary>
-    public class NeuralConfig
+    [JsonIgnore]
+    public List<InputNode> InputNodes { get; protected set; }
+    /// <summary>
+    /// The input nodes to use by the neural network, including the memory nodes
+    /// </summary>
+    [JsonIgnore]
+    public List<InputNode> AllInputNodes => InputNodes.Concat(InputMemoryNodes).ToList();
+    /// <summary>
+    /// The output nodes used by the neural network. 
+    /// </summary>
+    [JsonIgnore]
+    public List<OutputNode> OutputNodes { get; protected set; }
+    /// <summary>
+    /// The input nodes used by the neural network, including the memory nodes
+    /// </summary>
+    [JsonIgnore]
+    public List<OutputNode> AllOutputNodes => OutputNodes.Concat(OutputMemoryNodes).ToList();
+
+    /// <summary>
+    /// Stores whether or not an input or output node is enabled, based on the index of them.
+    /// </summary>
+    public bool[] EnabledStates { get; set; }
+
+    private readonly List<InputNode> InputMemoryNodes;
+    private readonly List<OutputNode> OutputMemoryNodes;
+
+    public NeuralConfig()
     {
-        /// <summary>
-        /// The input nodes to use by the neural network
-        /// </summary>
-        [JsonIgnore]
-        public List<InputNode> InputNodes { get; protected set; }
-        /// <summary>
-        /// The output nodes used by the neural network. 
-        /// </summary>
-        [JsonIgnore]
-        public List<OutputNode> OutputNodes { get; protected set; }
+        InputNodes = new();
+        OutputNodes = new();
+        EnabledStates = Array.Empty<bool>();
+        InputMemoryNodes = new();
+        OutputMemoryNodes = new();
 
-        /// <summary>
-        /// Stores whether or not an input or output node is enabled, based on the index of them.
-        /// </summary>
-        public bool[] EnabledStates { get; set; }
+        InitMemoryNodes();
+    }
 
-        public NeuralConfig()
+    /// <summary>
+    /// Returns the total amount of enabled input nodes, including all of the nodes of inputs that use a grid.
+    /// </summary>
+    /// <returns></returns>
+    public int GetInputCount()
+    {
+        int count = 0;
+
+        foreach (var input in InputNodes)
         {
-            InputNodes = new List<InputNode>();
-            OutputNodes = new List<OutputNode>();
-            EnabledStates = new bool[0];
+            if (input.ShouldUse) count += input.TotalWidth * input.TotalHeight;
         }
 
-        /// <summary>
-        /// Returns the total amount of enabled input nodes, including all of the nodes of inputs that use a grid.
-        /// </summary>
-        /// <returns></returns>
-        public int GetInputCount()
+        count += ShortTermMemoryNodeCount;
+        count += LongTermMemoryNodeCount;
+        count += PermanentMemoryNodeCount;
+
+        return count;
+    }
+
+    /// <summary>
+    /// Returns the total amount of enabled output nodes.
+    /// </summary>
+    /// <returns></returns>
+    public int GetOutputCount()
+    {
+        int count = 0;
+        foreach (var output in OutputNodes)
         {
-            int count = 0;
-
-            foreach (var input in InputNodes)
-            {
-                if (input.ShouldUse) count += input.TotalWidth * input.TotalHeight;
-            }
-
-            return count;
+            if (output.ShouldUse) count++;
         }
 
-        /// <summary>
-        /// Returns the total amount of enabled output nodes.
-        /// </summary>
-        /// <returns></returns>
-        public int GetOutputCount()
-        {
-            int count = 0;
-            foreach (var output in OutputNodes)
-            {
-                if (output.ShouldUse) count++;
-            }
+        count += ShortTermMemoryNodeCount;
+        count += LongTermMemoryNodeCount * 2;
+        count += PermanentMemoryNodeCount * 2;
 
-            return count;
-        }
+        return count;
+    }
 
-        public string Serialize() => JsonConvert.SerializeObject(this, SerializationUtils.JSON_CONFIG);
+    private void InitMemoryNodes()
+    {
+        OutputMemoryNodes.Add(new OutputNode("Short Term Memory", ShortTermMemoryNodeCount, 1));
+        OutputMemoryNodes.Add(new OutputNode("Long Term Memory", LongTermMemoryNodeCount, 2));
+        OutputMemoryNodes.Add(new OutputNode("Permanent Memory", PermanentMemoryNodeCount, 2));
+        InputMemoryNodes.Add(new InputNode("Short Term Memory", ShortTermMemoryNodeCount > 0, ShortTermMemoryNodeCount, 1));
+        InputMemoryNodes.Add(new InputNode("Long Term Memory", LongTermMemoryNodeCount > 0, LongTermMemoryNodeCount, 1));
+        InputMemoryNodes.Add(new InputNode("Permanent Memory", PermanentMemoryNodeCount > 0, PermanentMemoryNodeCount, 1));
+    }
 
-        public static NeuralConfig Deserialize(string json)
-        {
-            NeuralConfig cfg = JsonConvert.DeserializeObject<NeuralConfig>(json, SerializationUtils.JSON_CONFIG)!;
+    public string Serialize() => JsonConvert.SerializeObject(this, SerializationUtils.JSON_CONFIG);
 
-            return cfg;
-        }
+    public static NeuralConfig Deserialize(string json)
+    {
+        NeuralConfig cfg = JsonConvert.DeserializeObject<NeuralConfig>(json, SerializationUtils.JSON_CONFIG)!;
+        cfg.InitMemoryNodes();
+        return cfg;
     }
 }
