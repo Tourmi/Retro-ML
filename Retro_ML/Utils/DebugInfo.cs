d6d12a0ed8a75b965e241952ec724aa9093f9808
Tourmi <domino_b10@hotmail.com>
2022-07-07 18:06:54 -0400

    Added back-end DebugInfo implementation.
    Also added default info for input sent to emulator

---
+++ Retro_ML/Utils/DebugInfo.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace Retro_ML.Utils;
+/// <summary>
+/// Class used to store and read debug information. Useful for testing plugins.
+/// </summary>
+public static class DebugInfo
+{
+    private static bool isDebug = false;
+    private static readonly Mutex mutex = new();
+    private static readonly Dictionary<string, string> infos = new();
+
+    /// <summary>
+    /// Deletes all stored information
+    /// </summary>
+    [Conditional("DEBUG")]
+    public static void ClearInfo()
+    {
+        _ = mutex.WaitOne();
+        infos.Clear();
+        mutex.ReleaseMutex();
+    }
+
+    /// <summary>
+    /// Adds new information to preview.
+    /// </summary>
+    [Conditional("DEBUG")]
+    public static void AddInfo(string key, string value)
+    {
+        _ = mutex.WaitOne();
+        infos[key] = value;
+        mutex.ReleaseMutex();
+    }
+
+    /// <summary>
+    /// Returns all information to preview
+    /// </summary>
+    public static IEnumerable<KeyValuePair<string, string>> GetInfo()
+    {
+        if (IsDebug)
+        {
+            _ = mutex.WaitOne();
+            var res = infos.AsEnumerable();
+            mutex.ReleaseMutex();
+            return res;
+        }
+        else
+        {
+            return Enumerable.Empty<KeyValuePair<string, string>>();
+        }
+    }
+
+    /// <summary>
+    /// Returns true if running in debug mode
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsDebug
+    {
+        get
+        {
+            isDebug = false;
+            Debug.Assert(isDebug = true);
+            return isDebug;
+        }
+    }
+}
